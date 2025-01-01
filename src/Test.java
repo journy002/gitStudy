f7335d356eb3efb36f8e2ac907bddca4fd82058c
oms <journy002@naver.com>
2025-01-01 20:02:45 +0900

    로그인 브랜치에서 저장

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test");
+
+        System.out.println("로그인 브랜치에서 코드 작성");
     }
 }
